fff00970e4f058cd7c5dc86e577f43906cb7e7a4
Jesse Barnes <jessekathrynbarnes@gmail.com>
2019-08-13 20:47:13 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -14,13 +14,13 @@ class Application
     elsif req.path.match(/cart/)
       @@cart.each do |cart|
         resp.write "#{cart}\n"
+      end 
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
     else
       resp.write "Path Not Found"
     end
-  end
 
     resp.finish
   end
