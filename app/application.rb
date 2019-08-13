11b853011c896cd54e519cb65c2c032fdcdace9f
Jesse Barnes <jessekathrynbarnes@gmail.com>
2019-08-13 20:46:35 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -10,6 +10,7 @@ class Application
     if req.path.match(/items/)
       @@items.each do |item|
         resp.write "#{item}\n"
+      end
     elsif req.path.match(/cart/)
       @@cart.each do |cart|
         resp.write "#{cart}\n"
